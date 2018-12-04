adfc663d21a6c46d62834794a9a68d0b98c8c108
Виктор Гуляев <GulyaevW@gmail.com>
2018-12-04 13:31:17 +0300

    -

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -9,7 +9,11 @@ public class LevelGenerator : MonoBehaviour
     public GameObject bg_prefab;
     public GameObject[] tile_prefabs;
 
-    private Vector3[,] space = new Vector3[15, 8];
+    protected static int xLength = 15;
+    protected static int yLength = 8;
+    
+    private Vector3[,] space = new Vector3[xLength, yLength];
+
     private Tile target_first;
     private Tile target_second;
     public Tile[] tiles;
@@ -18,17 +22,17 @@ public class LevelGenerator : MonoBehaviour
 
     private void Start()
     {
-        tiles = new Tile[space.GetLength(0) * space.GetLength(1)];
-        float xOffset = space.GetLength(0) / 2f - 0.5f;
+        tiles = new Tile[xLength * space.GetLength(1)];
+        float xOffset = xLength / 2f - 0.5f;
         float yOffset = space.GetLength(1) / 2f - 0.5f;
 
         levelIndex = Random.Range(0, Levels.templates.Length);
 
         for (int y = 0; y < space.GetLength(1); y++)
         {
-            for (int x = 0; x < space.GetLength(0); x++)
+            for (int x = 0; x < xLength; x++)
             {
-                var index = x + (y * space.GetLength(0));
+                var index = x + (y * xLength);
                 if (Levels.templates[levelIndex][index] == 1)
                 {
                     var pos = new Vector3(x - xOffset, y - yOffset, 0);
@@ -51,15 +55,12 @@ public class LevelGenerator : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-
+        if (hit.collider.tag != "Player") return;
+        Debug.Log(1);
         if (Input.GetMouseButtonDown(0))
         {
-            if (hit.collider != null)
-            {
-                startMousePos = hit.point;
-                target_first = hit.collider.gameObject.GetComponent<Tile>();
-                Debug.Log("Start: " + hit.collider.name + " Type:" + target_first.typeIndex);
-            }
+            startMousePos = hit.point;
+            target_first = hit.collider.gameObject.GetComponent<Tile>();
         }
         else if (Input.GetMouseButtonUp(0))
         {
@@ -71,6 +72,7 @@ public class LevelGenerator : MonoBehaviour
                 if (startMousePos.x - hit.point.x < 0)
                 {
                     direction = Direction.Right;
+
                 }
                 else
                 {
@@ -88,22 +90,28 @@ public class LevelGenerator : MonoBehaviour
                     direction = Direction.Down;
                 }
             }
-            Tile target_second = SearchTile(target_first, direction);
-            if (target_second != null)
+            Debug.Log(direction.ToString());
+
+            Tile target_second = SearchTileToDiretion(target_first, direction);
+
+            if (target_first != null && target_second != null)
             {
-                Debug.Log("End: " + target_second.name + " Type:" + target_second.typeIndex);
-                if (target_first != null && target_second != null)
-                {
-                    StartCoroutine(NewPos(target_first, target_second));
-                }               
+                StartCoroutine(NewPos(target_first, target_second));
+                target_first = null;
+                target_second = null;
+            }
+            else
+            {
+                target_first = null;
+                target_second = null;
             }
         }
+
+        //tiles[0].transform.position = hit.point;
     }
 
-    private Tile SearchTile(Tile target_first, Direction direction)
+    private Tile SearchTileToDiretion(Tile target_first, Direction direction)
     {
-        int yLength = space.GetLength(1);
-        int xLength = space.GetLength(0);
         int indexX = target_first.indexX;
         int indexY = target_first.indexY;
         int searchIndexX = target_first.indexX;
@@ -112,34 +120,30 @@ public class LevelGenerator : MonoBehaviour
         {
             case Direction.Up:
                 searchIndexY += 1;
-                if (searchIndexY < yLength)
+                if (searchIndexY < yLength -2)
                 {
-                    var tile = tiles[searchIndexX + (searchIndexY * xLength)];
-                    return tile;
+                    return tiles[searchIndexX + (searchIndexY * xLength)];
                 }
                 return null;
             case Direction.Down:
                 searchIndexY -= 1;
                 if (searchIndexY >= 0)
                 {
-                    var tile = tiles[searchIndexX + (searchIndexY * xLength)];
-                    return tile;
+                    return tiles[searchIndexX + (searchIndexY * xLength)];
                 }
                 return null;
             case Direction.Right:
                 searchIndexX += 1;
                 if (searchIndexX < xLength)
                 {
-                    var tile = tiles[searchIndexX + (searchIndexY * xLength)];
-                    return tile;
+                    return tiles[searchIndexX + (searchIndexY * xLength)];
                 }
                 return null;
             case Direction.Left:
                 searchIndexX -= 1;
                 if (searchIndexX >= 0)
                 {
-                    var tile = tiles[searchIndexX + (searchIndexY * xLength)];
-                    return tile;
+                    return tiles[searchIndexX + (searchIndexY * xLength)];
                 }
                 return null;
         }
@@ -150,14 +154,20 @@ public class LevelGenerator : MonoBehaviour
     {
         float speed = 3f;
         float timer = 0f;
-        Vector2 start_first = target_first.transform.position;
-        Vector2 start_second = target_second.transform.position;
+
+        Tile tempTile = target_first;
+        tiles[target_first.indexX + (target_first.indexY * xLength)] = target_second;
+        tiles[target_second.indexX + (target_second.indexY * xLength)] = tempTile;
+
         int x = target_first.indexX;
         int y = target_first.indexY;
         target_first.indexX = target_second.indexX;
         target_first.indexY = target_second.indexY;
         target_second.indexX = x;
         target_second.indexY = y;
+
+        Vector2 start_first = target_first.transform.position;
+        Vector2 start_second = target_second.transform.position;
 
         while (timer < 1)
         {
@@ -166,6 +176,7 @@ public class LevelGenerator : MonoBehaviour
             target_second.transform.position = Vector3.Lerp(start_second, start_first, timer);            
             yield return null;
         }
+
         target_second.transform.position = start_first;
         target_first.transform.position = start_second;        
     }
